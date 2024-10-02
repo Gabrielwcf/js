248b4adca96784cc7907cf35991a29af5bd4d2b4
Gabrielwcf <gabrielfiuza6624@gmail.com>
2024-10-02 15:25:44 -0300

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,8 @@
+function togglePopup(popupId) {
+    const popup = document.getElementById(popupId);
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
---
+++ function togglePopup(popupId) {.js
@@ -0,0 +1,4 @@
+function togglePopup(popupId) {
+    const popup = document.getElementById(popupId);
+    popup.style.display = (popup.style.display === "block") ? "none" : "block";
+}
